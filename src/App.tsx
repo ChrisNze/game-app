7a09adcb120a29682ca02d74ff35857bc2e240a2
Chris <nzeanoruec@gmail.com>
2023-04-23 23:10:45 +0100

    Build in responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
 function App() {
-	return <Button colorScheme="blue">Button</Button>;
+	return (
+		<div className="container-fluid bg-primary">
+			<div className="row">
+				<div className="col bg-secondary">Nav</div>
+			</div>
+			<div className="row">
+				<div className="col-lg-6 bg-info">Aside</div>
+				<div className="col-lg-6 bg-success d-none d-lg-block">Main</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
