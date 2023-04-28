69e5af46d4b4109953e0b8252e2a9d21f32e5ef7
Chris <nzeanoruec@gmail.com>
2023-04-28 23:44:55 +0100

    Media Query

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game, darkMode }: Props) => {
 	return (
 		<>
-			<div className="col-6 col-md-4 col-lg-3 ">
+			<div className="col-4 col-md-4 col-lg-3 ">
 				<div className="card card_container" style={{ backgroundColor: darkMode ? "rgba(0,0,0,.2)" : "transparent" }}>
 					<img src={game.background_image} className="card-img-top" alt="Image" />
 					<div className="card-body">
